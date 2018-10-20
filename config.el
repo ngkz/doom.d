139ced727feea6e5d002ed89c14c0dd281be9b71
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2018-10-21 07:13:37 +0900

    highlight trailing whitespace

--- config.el
+++ config.el
@@ -5,7 +5,6 @@
       doom-variable-pitch-font (font-spec :family "Nasu")
       doom-unicode-font (font-spec :family "NasuM")
       doom-big-font (font-spec :family "NasuM" :size 22))
-
 (setq doom-one-brighter-comments t)
-
 (setq-default comment-start "# ")
+(setq-default show-trailing-whitespace t)
