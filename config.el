106cd6ccd8e167d91416744cde2c038356ad8aaf
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2018-10-20 15:45:27 +0900

    set doom-variable-pitch-font, doom-unicode-font, doom-big-font

--- config.el
+++ config.el
@@ -1,4 +1,7 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(setq doom-font (font-spec :family "NasuM" :size 14))
+(setq doom-font (font-spec :family "NasuM" :size 14)
+      doom-variable-pitch-font (font-spec :family "Nasu")
+      doom-unicode-font (font-spec :family "NasuM")
+      doom-big-font (font-spec :family "NasuM" :size 22))
