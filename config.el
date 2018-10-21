e16800acb20c2e5f733fd650b6bf1c1388af9de9
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2018-10-21 16:16:41 +0900

    quicker window resizing

--- config.el
+++ config.el
@@ -31,3 +31,50 @@
 
 ;soft wrapping
 (global-visual-line-mode t)
+
+;resize window quickly
+(defun my-window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally (* dx 4)))
+              ((= c ?L)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally (* dx 4)))
+              ((= c ?H)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window (* dy 4)))
+              ((= c ?J)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window (* dy 4)))
+              ((= c ?K)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))
+
+(map! :map evil-window-map
+      "SPC" #'my-window-resizer) ; CTRL-w SPC or SPC w SPC
