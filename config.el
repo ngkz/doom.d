7e74a67c686f415d1d409e16f3eeef9c7aa8fa49
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2018-10-21 11:35:51 +0900

    highlight tab, hard space, and full-width space

--- config.el
+++ config.el
@@ -8,3 +8,20 @@
 (setq doom-one-brighter-comments t)
 (setq-default comment-start "# ")
 (setq-default show-trailing-whitespace t)
+
+; highlight tab, hard space, and full-width space
+(require 'whitespace)
+(setq whitespace-style '(
+    face
+    tabs
+    spaces
+    space-mark
+    tab-mark
+))
+(setq whitespace-display-mappings '(
+    (space-mark ?\u3000 [?␣])       ;full-width space AMBIGUOUS WIDTH!
+    (space-mark ?\u00A0 [?\uFF65])   ;hard space
+    (tab-mark   ?\t     [?» ?\t])    ;tab
+))
+(setq whitespace-space-regexp "\\([\u3000]+\\)") ; highlight only full-width space
+(global-whitespace-mode t)
