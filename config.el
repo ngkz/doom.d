9e8b855c461f03b73d80c434a504532759c4ff07
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2019-01-13 09:38:29 +0900

    hidpi

--- config.el
+++ config.el
@@ -1,10 +1,16 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(setq doom-font (font-spec :family "NasuM" :size 14)
-      doom-variable-pitch-font (font-spec :family "Nasu")
-      doom-unicode-font (font-spec :family "NasuM")
-      doom-big-font (font-spec :family "NasuM" :size 22))
+(if (eq (system-name) "haramaki")
+    (setq doom-font (font-spec :family "NasuM" :size 20)
+          doom-variable-pitch-font (font-spec :family "Nasu")
+          doom-unicode-font (font-spec :family "NasuM")
+          doom-big-font (font-spec :family "NasuM" :size 24))
+  (setq doom-font (font-spec :family "NasuM" :size 14)
+        doom-variable-pitch-font (font-spec :family "Nasu")
+        doom-unicode-font (font-spec :family "NasuM")
+        doom-big-font (font-spec :family "NasuM" :size 22))
+  )
 (setq doom-one-brighter-comments t)
 ;; don't lighten the background of the comment
 (setq doom-one-comment-bg nil)
