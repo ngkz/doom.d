0aced251357bf0695719b605aaedea163454b32e
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2019-03-14 21:01:42 +0900

    unset org-agenda-files

--- config.el
+++ config.el
@@ -146,7 +146,5 @@
 ;; Make sure that the weekdays in the time stamps of your Org mode files and in the agenda appear in English.
 (setq system-time-locale "C")
 
-(setq org-agenda-files (list "~/Dropbox2/Dropbox/org/todo.org"))
-
 ;; https://stackoverflow.com/questions/20164918/how-to-untick-checkboxes-in-org-mode-for-the-next-cyclic-repetitive-task
 (require 'org-checklist)
